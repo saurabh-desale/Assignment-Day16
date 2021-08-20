cfb42a792fcdd88b10a535909cfdbccf156efe7f
Saurabh <saurabhdesale007@gmail.com>
2021-08-20 12:09:32 +0530

    Modified: Insersion Sort Logic

--- com/blz/assignment/AlgorithumPb/InsersionSort03/InsersionSort.java
+++ com/blz/assignment/AlgorithumPb/InsersionSort03/InsersionSort.java
@@ -1,21 +1,31 @@
 package com.blz.assignment.AlgorithumPb.InsersionSort03;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class InsersionSort {
+
 	public static void main(String[] args) {
-		List<String> list = new ArrayList<>();
-		list.add("Shubham");
-		list.add("Saurabh");
-		list.add("Bazeela");
-		list.add("Pallavi");
-		list.add("Bittu");
-		list.add("Rudra");
-		System.out.println("before sorting: \n"+list);
-		Collections.sort(list);
-		System.out.println("after sorting: \n"+list);
 		
+	String array[] = {"papu","Bunty","Bubly","jamun","rocky","Bheem"};
+	int n = array.length;
+	System.out.println("before sorting: ");	
+	for(String item: array) {
+		System.out.print(item+" ");
+	}
+	System.out.println();
+	
+	for (int i =1; i<n ; i++) {
+		String temp = array[i];
+		int j = i-1;
+		while(j >= 0 && array[j].compareTo(temp)>0) {
+			array[j+1] = array[j];
+			j--;
+		}
+		array[j+1]=temp;
+	}
+	
+	System.out.println("after sorting: ");	
+	for(String item: array) {
+		System.out.print(item+" ");
+	}
 	}
 }
