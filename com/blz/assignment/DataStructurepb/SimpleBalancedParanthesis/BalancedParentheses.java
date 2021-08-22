47bbd52442c4eaecc532591028c689c94e2114c8
Saurabh <saurabhdesale007@gmail.com>
2021-08-22 10:29:09 +0530

    Modidfied: applied some oops concept

--- com/blz/assignment/DataStructurepb/SimpleBalancedParanthesis/BalancedParentheses.java
+++ com/blz/assignment/DataStructurepb/SimpleBalancedParanthesis/BalancedParentheses.java
@@ -1,51 +1,13 @@
 package com.blz.assignment.DataStructurepb.SimpleBalancedParanthesis;
 
-import java.util.Stack;
-
 public class BalancedParentheses {
 
-	private static boolean checkBalancedParentheses(String input) {
-		Stack<String> stack = new Stack<String>();
-		boolean isBalanaced = false;
-
-		for (int i = 0; i < input.length(); i++) {
-			String str = "" + input.charAt(i);
-
-			if (str.equals("(") || str.equals("[") || str.equals("{")) {
-				stack.push(str);
-			}
-
-			if (str.equals(")") || str.equals("]") || str.equals("}")) {
-
-				if (stack.isEmpty()) {
-					return false;
-				}
-				String opening = stack.peek();
-				if (str.equals(")") && opening.equals("(")) {
-					stack.pop();
-				}
-				if (str.equals("]") && opening.equals("[")) {
-					stack.pop();
-				}
-				if (str.equals("}") && opening.equals("{")) {
-					stack.pop();
-				}
-			}
-		}
-
-		if (input.length() > 0 && stack.isEmpty()) {
-			isBalanaced = true;
-		}
-
-		return isBalanaced;
-	}
-
 	public static void main(String[] args) {
-
+		IMethods obj = new Operations();
 		String[] inputs = { "(5+6)*(7+8)/(4+3)(5+6)*(7+8)/(4+3)" };
 
 		for (String input : inputs) {
-			boolean flag = checkBalancedParentheses(input);
+			boolean flag = obj.checkBalancedParentheses(input);
 			if (flag) {
 				System.out.println("Balanced");
 			} else {
